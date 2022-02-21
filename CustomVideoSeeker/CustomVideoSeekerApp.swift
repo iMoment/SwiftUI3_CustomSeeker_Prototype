42a38d463c2fc2528e594b181134c529e247a0c6
Stanley Pan <imoment.developer@gmail.com>
2022-02-21 18:29:19 -0500

    Initial Commit

---
+++ CustomVideoSeeker/CustomVideoSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomVideoSeekerApp.swift
+//  CustomVideoSeeker
+//
+//  Created by Stanley Pan on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomVideoSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
